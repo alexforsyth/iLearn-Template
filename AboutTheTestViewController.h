a95806c123848d29c263a45e775f95829b86a1fd
forsytha <forsytha@umich.edu>
2015-04-02 12:44:57 -0400

    comments

--- AboutTheTestViewController.h
+++ AboutTheTestViewController.h
@@ -5,6 +5,10 @@
 //  Created by Alexander Forsyth on 6/15/14.
 //  Copyright (c) 2014 Alexander Forsyth. All rights reserved.
 //
+// THIS IS A FILE THAT IS "ABOUT THE TEST" view controller
+// stays within naming conventions
+// does not provide any information on "about" anything
+
 
 #import <UIKit/UIKit.h>
 
